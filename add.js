4eb8ecb9dc9ceb33a4c42b6dfe3e7b3e99b4cd51
Vadim <wazzupvadim@gmail.com>
2020-02-09 15:19:39 +0200

    added js

--- add.js
+++ add.js
@@ -1,26 +1,53 @@
-let classSelector,
-    classElements,
-    startActive;
 
-classSelector = '.liked';
-
-classElements = document.querySelectorAll(classSelector);
-
-startActive = function() {
-  for (let i = 0; i < classElements.length; i++) {
-    this.classList.add('liked1');
-  }  
-
-};
-
-for (let i = 0; i < classElements.length; i++) {
-  classElements[i].addEventListener('mousedown', startActive);
-}
+let likeButtons = document.querySelectorAll(".like")
 
 
+  for (let i = 0; i < likeButtons.length; i++) {
+    likeButtons[i].addEventListener("click", function(){
+    	likeButtons[i].classList.toggle("liked")
+    })
+  } 
+	let minCount = 1;
+	let maxCount = 5;
+
+	function toggleButtonState(count) {
+					decrementBtn.disabled = count <=minCount;
+					incrementBtn.disabled = count >=maxCount;
+			}
+
+	let decrementBtn = document.querySelectorAll(".decrement-btn")[0],
+	    incrementBtn = document.querySelectorAll(".increment-btn")[0],
+		quaintityInput = document.querySelectorAll(".product-quantity input")[0];
+
+		let currentValue = +quaintityInput.value;
+			
+			toggleButtonState(currentValue)
+
+		incrementBtn.addEventListener("click", function() {
+			let currentValue = +quaintityInput.value;
+			let nextValue = currentValue+1;
+			quaintityInput.value = nextValue;
+			toggleButtonState(nextValue)
+		})
+			decrementBtn.addEventListener("click", function() {
+			let currentValue = +quaintityInput.value;
+			let nextValue = currentValue-1;
+			quaintityInput.value = nextValue;
+			
+			toggleButtonState(nextValue)
+		})
 
 
 
+
+
+
+
+
+
+
+
+ 
 
 
 
